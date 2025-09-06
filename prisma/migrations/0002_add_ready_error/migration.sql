1fb120ac40812818acc823f202ea366dd3452869
worlexio <info@worlex.io>
2025-09-06 10:56:57 +0300

    fix(prisma): remove UTF-8 BOM from 0002_add_ready_error migration to prevent P3018; unblock deploy

--- prisma/migrations/0002_add_ready_error/migration.sql
+++ prisma/migrations/0002_add_ready_error/migration.sql
@@ -1,4 +1,4 @@
-﻿-- Alter enum InvoiceStatus to add Ready and Error
+-- Alter enum InvoiceStatus to add Ready and Error
 DO $$ BEGIN
   ALTER TYPE "InvoiceStatus" ADD VALUE IF NOT EXISTS 'Ready';
   ALTER TYPE "InvoiceStatus" ADD VALUE IF NOT EXISTS 'Error';
